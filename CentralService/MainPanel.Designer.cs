b48fafd9bc00e82a5021757c371a4b736e04cf4b
ranallor <ranallra2020@mountunion.edu>
2018-12-04 09:31:03 -0500

    Added starting files

---
+++ CentralService/MainPanel.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CentralService
+{
+    partial class statusWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.startButton = new System.Windows.Forms.Button();
+            this.editLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serverLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(3, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(724, 327);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Service Log";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(21, 21);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(685, 275);
+            this.textBox3.TabIndex = 3;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(24, 405);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(135, 36);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // editLabel
+            // 
+            this.editLabel.AutoSize = true;
+            this.editLabel.Location = new System.Drawing.Point(107, 25);
+            this.editLabel.Name = "editLabel";
+            this.editLabel.Size = new System.Drawing.Size(49, 17);
+            this.editLabel.TabIndex = 6;
+            this.editLabel.Text = "Offline";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(358, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(297, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Central Service Control";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "User(s) Number:";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(121, 42);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(16, 17);
+            this.numberLabel.TabIndex = 10;
+            this.numberLabel.Text = "0";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(574, 405);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(135, 36);
+            this.stopButton.TabIndex = 11;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 36);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // serverLabel
+            // 
+            this.serverLabel.AutoSize = true;
+            this.serverLabel.Location = new System.Drawing.Point(12, 25);
+            this.serverLabel.Name = "serverLabel";
+            this.serverLabel.Size = new System.Drawing.Size(98, 17);
+            this.serverLabel.TabIndex = 13;
+            this.serverLabel.Text = "Server Status:";
+            // 
+            // statusWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 453);
+            this.Controls.Add(this.serverLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.editLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "statusWindow";
+            this.Text = "Central Service";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label editLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label serverLabel;
+    }
+}
+
---
+++ CentralService/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CentralService
+{
+    class Service
+    {
+        Socket serviceSocket;
+        int port = 100;
+        int backLog = 5;
+
+        public Service()
+        {
+            serviceSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+        }
+
+        public void start()
+        {
+            serviceSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            serviceSocket.Listen(backLog);
+        }
+    }
+}
